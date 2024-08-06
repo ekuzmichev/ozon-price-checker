c293580b3a5de6b3d1878e8f74d42339b444e704
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-08-06 23:18:23 +0300

    Send message only if price reaches threshold

--- src/main/scala/consumer/ProductWatchingJobSchedulerImpl.scala
+++ src/main/scala/consumer/ProductWatchingJobSchedulerImpl.scala
@@ -1,8 +1,9 @@
 package ru.ekuzmichev
 package consumer
-import product.ProductFetcher
+import common.ProductId
+import product.{ProductFetcher, ProductInfo}
 import schedule.Cron4sNextTimeProvider.fromCronString
-import schedule.ZioScheduler
+import schedule.{NextTimeProvider, ZioScheduler}
 import store.ProductStore
 import store.ProductStore.{Product, SourceId}
 import util.telegram.MessageSendingUtils.sendTextMessage
@@ -22,30 +23,45 @@ class ProductWatchingJobSchedulerImpl(
   private implicit val _telegramClient: TelegramClient = telegramClient
 
   override def scheduleProductsWatching(sourceId: ProductStore.SourceId): Task[Unit] =
+    initNextTimeProvider().flatMap {
+      zioScheduler
+        .schedule(makeProductWatchJob(sourceId), _, s"${sourceId.userName}|${sourceId.chatId}")
+        .forkDaemon
+        .tap(productStore.addScheduleFiberRuntime(sourceId, _))
+    }.ignore
+
+  private def makeProductWatchJob(sourceId: SourceId) =
+    productStore
+      .readSourceState(sourceId)
+      .map(_.fold(Seq.empty[Product])(_.products))
+      .flatMap { products =>
+        fetchProductInfos(products)
+          .flatMap { productInfos =>
+            val productsReachedThreshold: Seq[Product] =
+              products
+                .filter(product => productInfos.get(product.id).exists(_.price <= product.priceThreshold))
+            ZIO.when(productsReachedThreshold.nonEmpty)(
+              sendTextMessage(
+                sourceId.chatId,
+                s"Here are your products that have reached price threshold:\n\n" +
+                  s"${productsReachedThreshold.zipWithIndex
+                      .map { case (product, index) =>
+                        s"${index + 1}) ${product.id}\n\t" +
+                          s"Current price: ${productInfos.get(product.id).map(_.price).getOrElse(Double.NaN)} ₽\n\t" +
+                          s"Price threshold: ${product.priceThreshold} ₽"
+                      }
+                      .mkString("\n")}"
+              )
+            )
+          }
+      }
+
+  private def fetchProductInfos(products: Seq[Product]): Task[Map[ProductId, ProductInfo]] =
+    ZIO
+      .foreach(products)(product => ZIO.attempt(product.id -> productFetcher.fetchProductInfo(product.id)))
+      .map(_.toMap)
+
+  private def initNextTimeProvider(): ZIO[Any, Throwable, NextTimeProvider] =
     ZIO
       .fromTry(fromCronString(cron))
       .logged("create cron4s time provider", printResult = _.info)
-      .flatMap {
-        zioScheduler
-          .schedule(
-            productStore
-              .readSourceState(sourceId)
-              .map(_.fold(Seq.empty[Product])(_.products))
-              .flatMap { products =>
-                sendTextMessage(
-                  sourceId.chatId,
-                  s"Here are your watched products:\n\n" +
-                    s"${products.zipWithIndex
-                        .map { case (product, index) =>
-                          s"${index + 1}) ${product.id}\n\t${product.priceThreshold} ₽"
-                        }
-                        .mkString("\n")}"
-                )
-              },
-            _,
-            s"${sourceId.userName}|${sourceId.chatId}"
-          )
-          .forkDaemon
-          .tap(productStore.addScheduleFiberRuntime(sourceId, _))
-      }
-      .ignore
