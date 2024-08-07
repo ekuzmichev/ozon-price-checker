2aeee32b97a3b02d6825de84ca5ed60e5c4abd1c
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-08-07 22:38:46 +0300

    Format code

--- src/test/scala/encryption/DecryptionTestApp.scala
+++ src/test/scala/encryption/DecryptionTestApp.scala
@@ -8,12 +8,11 @@ object DecryptionTestApp extends ZIOAppDefault:
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] = zio.Runtime.removeDefaultLoggers >>> SLF4J.slf4j
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
-    (for {
-      text               <- Console.readLine("Text to decrypt: ")
-      encDec             <- ZIO.service[EncDec]
-      decryptedText      <- encDec.decrypt(text)
-      _                  <- ZIO.log(s"Decrypted text: $decryptedText")
-    } yield ()).provideLayer(
+    (for
+      text          <- Console.readLine("Text to decrypt: ")
+      encDec        <- ZIO.service[EncDec]
+      decryptedText <- encDec.decrypt(text)
+      _             <- ZIO.log(s"Decrypted text: $decryptedText")
+    yield ()).provideLayer(
       ZLayer.fromZIO(Console.readLine("Encryption password: ")).flatMap(env => EncDecLayers.aes256(env.get))
     )
-    
